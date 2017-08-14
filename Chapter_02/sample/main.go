5548e0c961c849823b452e62363ccaae211d04cd
Maximillian Arruda <dearrudam@gmail.com>
2017-08-13 23:01:46 -0300

    Added some comments

--- Chapter_02/sample/main.go
+++ Chapter_02/sample/main.go
@@ -7,11 +7,14 @@ import (
 	"github.com/dearrudam/GoInActionStudies/Chapter_02/sample/search"
 )
 
+//init is called prior to main
 func init() {
+	// set the device for logging  to stdout
 	log.SetOutput(os.Stdout)
 }
 
-func main()  {
-
+//main is the entry point for the program
+func main() {
+	//perform the search for the specific term
 	search.Run("president")
 }
--- Chapter_02/sample/search/search.go
+++ Chapter_02/sample/search/search.go
@@ -6,43 +6,60 @@ import (
 	"fmt"
 )
 
+// A map of registered matchers for searching
 var matchers = make(map[string]Matcher)
 
+// A function responsible to register a given matcher with a given name
 func Register(name string, matcher Matcher) {
 	matchers[name] = matcher
 }
 
+// Run performs the search logic
 func Run(searchTerm string) {
 
+	// Retrieve the list of feeds to search through
 	feeds, err := RetrieveFeeds()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Create a unbuffered channel to receive match results
 	results := make(chan *Result)
 
+	// Setup a wait group so we can process all the feeds
 	var waitGroup sync.WaitGroup
 
+	// Set the number of goroutines we need to wait for while
+	// they process the individual feeds
 	waitGroup.Add(len(feeds))
 
+	// Launch a goroutine for each feed to find the results.
 	for _, feed := range feeds {
 
+		// Retrieve a matcher for the search
 		matcher, exists := matchers[feed.Type]
-
 		if !exists {
 			matcher = matchers["default"]
 		}
 
+		// Launch the goroutine to perform the search
 		go func(matcher Matcher, feed *Feed) {
 			Match(matcher, feed, searchTerm, results)
 			waitGroup.Done()
 		}(matcher, feed)
 	}
 
+	// Launch a goroutine to monitor when all the work is done
 	go func() {
+		// Wait for everything to be processed
 		waitGroup.Wait()
+		// Close the channel to signal to the Display
+		// function that we can exit the program
 		close(results)
 	}()
 
+	// Start displaying results as they are available and
+	// result after the final result is displayed
 	Display(results)
 }
 
